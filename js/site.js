3007c31d32e22ede91870dae7369a11a67df62c3
Giles Dring <gilesdring@gmail.com>
2016-04-05 23:05:31 +0200

    Streamlining js

---
+++ js/site.js
@@ -0,0 +1,19 @@
+---
+---
+// JQuery
+{% include js/jquery-1.11.0.js %}
+
+// Bootstrap Core JavaScript
+{% include js/bootstrap.min.js %}
+
+// Plugin JavaScript
+{% include js/jquery.easing.min.js %}
+{% include js/classie.js %}
+{% include js/cbpAnimatedHeader.js %}
+
+// Contact Form JavaScript
+{% include js/jqBootstrapValidation.js %}
+{% include js/contact_me.js %}
+
+// Custom Theme JavaScript
+{% include js/agency.js %}
