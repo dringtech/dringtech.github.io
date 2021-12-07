6d1bca5c26f03be85722ecb3fab8db15f4706ec2
Giles Dring <giles@dringtech.com>
2021-12-07 18:08:24 +0000

    Style cookie message

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3fb114',
+      },
+    },
   },
   variants: {
     extend: {},
