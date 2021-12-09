b41fb4b9bcf427529f855e536ef9dbf7bcb31824
Giles Dring <giles@dringtech.com>
2021-12-09 09:23:25 +0000

    Major unpicking of loads of stuff
    Removed all sass components and most js apart from custom.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
 module.exports = {
-  purge: [
-      // './_*/**/*.html',
-      // './_src/**/*.css',
-  ],
+  purge: {
+    enabled: true,
+    content: [
+      './*.html',
+      './!(_site|node_modules)/**/*.html',
+      './_src/**/*.css',
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ['IBM Plex Sans', 'sans-serif'],
+      heading: ['IBM Plex Sans Condensed', 'sans-serif'],
+    },
     extend: {
       colors: {
-        primary: '#3fb114',
+        primary: 'var(--color-primary)',
+        'primary-highlight': 'var(--color-primary-highlight)',
       },
+      fontSize: {
+        'document': '20px',
+      },
+      letterSpacing: {
+        'cavernous': '0.2em',
+      },
+      zIndex: {
+        '-1': '-1',
+       }
     },
   },
   variants: {
